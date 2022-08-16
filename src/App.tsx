3126de0279e524d81b9bf111527ad91c0e043535
Molitvin Yevhenii <molitvin.ye@gmail.com>
2022-08-16 12:47:44 +0300

    Pressing on the Project card selects the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,29 @@
-import React, { useCallback } from 'react'
+import React, { useState, useCallback, useMemo } from 'react'
 import { View } from 'react-native'
+import isEqual from 'lodash.isequal'
 
 import { Project } from 'app/types/Project'
+import { Task } from './types/Task'
 import { ProjectsListView } from 'app/widgets/ProjectsList'
 import { TasksListView } from './widgets/TasksList'
 import { PROJECTS } from 'app/mock/data'
-import { Task } from './types/Task'
 
 import styles from './App.styles'
 
 function App() {
+  const [projects, setProjects] = useState<Project[]>(PROJECTS)
+
+  const defaultTasks = useMemo(() => {
+    return projects.map(project => project.tasks).flat()
+  }, [])
+
+  const [tasks, setTasks] = useState<Task[]>(defaultTasks)
+
   const onProjectPress = useCallback((project: Project) => {
-    // TODO
+    setTasks(prev => {
+      const isSecondPress = isEqual(prev, project.tasks)
+      return isSecondPress ? defaultTasks : project.tasks
+    })
   }, [])
 
   const onTaskPress = useCallback((task: Task) => {
@@ -21,13 +33,10 @@ function App() {
   return (
     <View style={styles.container}>
       <View style={styles.column}>
-        <ProjectsListView projects={PROJECTS} onProjectPress={onProjectPress} />
+        <ProjectsListView projects={projects} onProjectPress={onProjectPress} />
       </View>
       <View style={styles.column}>
-        <TasksListView
-          tasks={PROJECTS.map(project => project.tasks).flat()}
-          onTaskPress={onTaskPress}
-        />
+        <TasksListView tasks={tasks} onTaskPress={onTaskPress} />
       </View>
     </View>
   )
